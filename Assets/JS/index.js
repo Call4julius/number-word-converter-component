f243e5cca82f2075d7454a716da7fbd999cf4c08
Julius Uwuigbe <call4julius@gmail.com>
2025-12-17 21:21:12 +0100

    Made some useful UI changes in CSS \nComplete JS with input modification with commas

--- Assets/JS/index.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("button");
-const entry = document.querySelector(".entry");
-const result = document.querySelector(".result");
-
-btn.addEventListener("click", () => {
-  entry.classList.add("hidden");
-  result.classList.remove("hidden", "d-flex");
-});
---
+++ Assets/JS/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const form = document.getElementById('form');
+const input = document.getElementById('amount');
+const deposit = document.querySelector('.deposit-btn');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
+
+// Stop space from being entered.
+input.addEventListener('keydown', function (e) {
+  if (e.code === 'Space' || e.key === ' ') {
+    e.preventDefault();
+  }
+});
+
+input.addEventListener('input', (e) => {
+  // Ensures only numbers can be entered in the text-input field.
+  if (isNaN(input.value.slice(-1)))
+    return (input.value = input.value.slice(0, -1));
+
+  // Prevent pasting spaces and remove all spaces from the value.
+  if (input.value.includes(' '))
+    return (input.value = input.value.replace(/\s+/g, ''));
+
+  const inputArray = input.value.replace(/,/g, '').split('').reverse();
+  const commaInputArray = [];
+
+  inputArray.forEach((_, i) => {
+    if (i % 3 === 0 && i > 0) commaInputArray.unshift(',');
+    commaInputArray.unshift(inputArray[i]);
+  });
+
+  input.value = commaInputArray.join('');
+});
